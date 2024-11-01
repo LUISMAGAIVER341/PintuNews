0ee4e71311aea937892c8fa5ac712a7c62d6a04a
LUISMAGAIVER341 <magaiverapple@outlook.com>
2024-11-01 13:47:52 -0300

    Opaaa

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: ['./src/**/*.html'],
+    darkMode: media, // ou 'media' ou 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
